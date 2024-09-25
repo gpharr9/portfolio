f3ce02fb7f3ff668a6b1529647e5929881189772
gpharr9 <gpharr9@gmail.com>
2024-09-25 10:03:07 -0500

    Navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,22 @@
+<!-- eslint-disable prettier/prettier -->
+<template>
+    <div class="nav">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "nav-bar",
+});
+</script>
+
+<style scoped>
+.nav {
+  background-color: red;
+  height: 3vh;
+}
+</style>
