2cda4bdcd8edfabb6fe49a5c5378f8345b137556
gpharr9 <gpharr9@gmail.com>
2024-09-25 09:27:33 -0500

    adding dist subtree

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,11 @@
-
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Hi! My name is Graham Pharr</h1>
+    <h2>
+      I am a full-stack software engineer with over a year of experience, I
+      graduated with a bachelors degree from ISU in 2023, and have been working
+      ever since
+    </h2>
   </div>
 </template>
 
