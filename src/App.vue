6c9e2c4a3d60d0557f5fd55c1f9db368b9663741
gpharr9 <gpharr9@gmail.com>
2024-09-25 10:02:53 -0500

    Starting proper route implementation

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <nav-bar></nav-bar>
   <router-view />
 </template>
+
+<script>
+import { defineComponent } from "vue";
+import NavBar from "./components/NavBar.vue";
+export default defineComponent({
+  name: "App",
+  components: {
+    NavBar,
+  },
+});
+</script>
 
 <style>
 #app {
